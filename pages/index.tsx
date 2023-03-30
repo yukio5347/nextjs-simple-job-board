2e79bd43b19b606be5a043fd8f227110fb132939
Yukio <yukio5347@gmail.com>
2023-03-30 21:34:49 +0900

    apply JobPostingItem component to pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,16 @@ import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { PrismaClient } from '@prisma/client';
 import Layout from '@/components/Layout';
-import { Map, Money } from '@/components/Icons';
+import JobPostingItem from "@/components/JobPostingItem";
 import JobPosting from '@/models/JobPosting';
+import { where, orderBy } from '@/lib/queries';
 import { __ } from '@/lib/helpers';
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const prisma = new PrismaClient();
   const params = await prisma.jobPosting.findMany({
-    where: {
-      closedAt: {
-        gte: new Date(),
-      },
-    },
-    orderBy: {
-      id: 'desc',
-    },
+    where,
+    orderBy,
     take: 20,
   });
   const jobPostings = JSON.parse(JSON.stringify(params.map(param => new JobPosting(param))));
@@ -34,30 +29,7 @@ const Home = ({ jobPostings }: { jobPostings: JobPosting[] }) => {
           <>
             <div className="grid gap-5 md:grid-cols-2">
               {jobPostings.map((jobPosting: JobPosting) => (
-                <Link key={jobPosting.id} href={`/jobs/${jobPosting.id}`} className="flex flex-col justify-between p-4 border rounded-lg transition-colors lg:hover:border-sky-500">
-                  <div className="flex-1">
-                    <h3 className="font-semibold leading-tight mb-1">{jobPosting.title}</h3>
-                    <p className="text-sm text-sky-500 font-semibold mb-2">{jobPosting.companyName}</p>
-                  {jobPosting.shortWorkPlace &&
-                    <p className="flex items-center text-xs text-gray-500 mb-1 home:lg:text-sm">
-                      <Map /> {jobPosting.shortWorkPlace}
-                    </p>
-                  }
-                  {jobPosting.shortSalary &&
-                    <p className="flex items-center text-xs text-gray-500 mb-1">
-                      <Money /> {jobPosting.shortSalary}
-                    </p>
-                  }
-                  </div>
-                  <div className="mt-3 flex justify-between items-center text-xs">
-                    <span className={`${jobPosting.employmentTypeColor} rounded font-medium py-1 px-2`}>
-                      {jobPosting.employmentTypeText}
-                    </span>
-                    <div>
-                      {jobPosting.createdAt.replace(/T.*/, '')}
-                    </div>
-                  </div>
-                </Link>
+                <JobPostingItem key={jobPosting.id} jobPosting={jobPosting} />
               ))}
             </div>
             <div className="my-10 text-center">
