3853ae4116682b0a33a19ad7614dfac8546dff22
Yukio <yukio5347@gmail.com>
2023-04-01 15:35:04 +0900

    add 'deletedAt: null' to where helper

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,5 +1,8 @@
 import { Prisma } from "@prisma/client";
 
-export const where: Prisma.JobPostingWhereInput = { closedAt: { gte: new Date() } }
+export const where: Prisma.JobPostingWhereInput = {
+  closedAt: { gte: new Date() },
+  deletedAt: null,
+}
 
 export const orderBy: Prisma.JobPostingOrderByWithRelationInput = { id: 'desc' };
