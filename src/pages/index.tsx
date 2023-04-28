aee76d37b6a5800d2709f4b304430e26db37a276
Yukio <yukio5347@gmail.com>
2023-04-28 22:56:28 +0900

    add Loading component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import JobPostingItem from '@/components/JobPostingItem';
 import Layout from '@/components/Layout';
+import Loading from '@/components/Loading';
 import Modal from '@/components/Modal';
 import { __ } from '@/lib/helpers';
 import { useJobList } from '@/lib/swr';
@@ -28,22 +29,7 @@ const Home = () => {
       {error ? (
         <>{error.message}</>
       ) : isLoading ? (
-        <div className='flex items-center'>
-          <svg
-            className='animate-spin mr-2 h-5 w-5 text-gray-500'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-          >
-            <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-            <path
-              className='opacity-75'
-              fill='currentColor'
-              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-            ></path>
-          </svg>
-          <span className='text-gray-500 text-sm'>Loading...</span>
-        </div>
+        <Loading />
       ) : jobPostings ? (
         <>
           <div className='grid gap-5 md:grid-cols-2'>
