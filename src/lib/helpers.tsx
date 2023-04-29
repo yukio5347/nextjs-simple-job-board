72824d4a156864b5f7e8a8b93aac7c49e66fb5f0
Yukio <yukio5347@gmail.com>
2023-04-29 17:35:19 +0900

    add Alert component

--- src/lib/helpers.tsx
+++ src/lib/helpers.tsx
@@ -25,3 +25,7 @@ export const addDays = (date: Date, days: number): Date => {
 export const serialize = (obj?: object | null) => {
   return JSON.parse(JSON.stringify(obj));
 };
+
+export const getErrorMessage = (error: Error | unknown): string => {
+  return error instanceof Error ? error.message : 'Unknown error occurred.. Please reload the page and try again.';
+};
