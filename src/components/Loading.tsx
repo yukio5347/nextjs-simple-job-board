6b61080fd4a9bbaaff2324728b1f33282694fe0e
Yukio <yukio5347@gmail.com>
2023-04-29 16:32:57 +0900

    add Spin icon component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,20 +1,10 @@
+import { Spin } from '@/components/Icons';
+
 export default function Loading() {
   return (
-    <div className='flex items-center'>
-      <svg
-        className='animate-spin mr-2 h-5 w-5 text-gray-500'
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-      >
-        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-        <path
-          className='opacity-75'
-          fill='currentColor'
-          d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-        ></path>
-      </svg>
-      <span className='text-gray-500 text-sm'>Loading...</span>
+    <div className='flex items-center text-gray-500'>
+      <Spin className='mr-2' />
+      <span className='text-sm'>Loading...</span>
     </div>
   );
 }
